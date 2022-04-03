1e9ea4037bcb9bf6fb9c1d1f9841bdc4f96f8f12
Eric MULLER <eric.pierre.muller@gmail.com>
2022-04-03 15:41:05 +0200

    # useless comments

--- src/main/java/com/emu/apps/admin/web/security/WebSecurityConfig.java
+++ src/main/java/com/emu/apps/admin/web/security/WebSecurityConfig.java
@@ -23,34 +23,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.adminServer = adminServer;
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-////        http
-////                .formLogin()
-////                .loginPage("/login.html")
-////                .loginProcessingUrl("/login")
-////                .permitAll();
-////        http
-////                .logout()
-////                .logoutUrl("/logout");
-////        http
-////                .csrf()
-////                .disable();
-////        http
-////                .authorizeRequests()
-////                .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
-////                .permitAll();
-////        http
-////                .authorizeRequests()
-////                .antMatchers("/**")
-////                .authenticated();
-////        http.httpBasic();
-//
-//        http.authorizeRequests().anyRequest().permitAll()
-//                .and().csrf().disable();
-//
-//    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         SavedRequestAwareAuthenticationSuccessHandler successHandler =
